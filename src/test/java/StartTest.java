2e9b6a6eac39d66b705cf19f89c625b8484d3567
webinartelran <webinartelran@gmail.com>
2023-05-29 20:57:21 +0300

    CW2

---
+++ src/test/java/StartTest.java
@@ -0,0 +1,28 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class StartTest {
+
+    WebDriver wd;
+
+    @BeforeTest
+    public void precondition(){
+        wd = new ChromeDriver();
+        wd.get("https://telranedu.web.app/home");
+// navigate to the link
+    }
+
+    @Test
+    public void testMethod(){
+// test login
+    }
+
+    @AfterTest
+    public void postcondition(){
+// click logout
+    }
+
+}
